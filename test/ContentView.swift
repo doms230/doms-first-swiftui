f15a3147a272326d33a1d93578ab9847cc5c64e8
Dominic Smith <doms230@live.com>
2020-09-07 12:45:19 -0400

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Dominic  Smith on 9/7/20.
+//  Copyright © 2020 Soundbrew, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
