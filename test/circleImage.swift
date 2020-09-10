ac1189fce441a464a1b395c3d67c4a1105e1ee3b
Dominic Smith <doms230@live.com>
2020-09-10 12:46:51 -0400

    first commit

---
+++ test/circleImage.swift
@@ -0,0 +1,21 @@
+//
+//  circleImage.swift
+//  test
+//
+//  Created by Dominic  Smith on 9/7/20.
+//  Copyright © 2020 Soundbrew, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct circleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct circleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        circleImage()
+    }
+}
---
+++ test/mapView.swift
@@ -0,0 +1,21 @@
+//
+//  mapView.swift
+//  test
+//
+//  Created by Dominic  Smith on 9/7/20.
+//  Copyright © 2020 Soundbrew, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct mapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct mapView_Previews: PreviewProvider {
+    static var previews: some View {
+        mapView()
+    }
+}
